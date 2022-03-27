a4c91b954bdfaa2b6f44c4d2c1782d2bd5d0bd66
owati <mofiyinfoluwaowati@gmail.com>
2022-03-27 19:03:57 +0100

    creating the nav bar component

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,20 +1,39 @@
 
 import '../css/navbar.css';
+import logo from '../assets/logo2.png';
+import search from '../assets/search.png';
 
 function NavBar () {
     return (
         <nav className='nav'>
             <div style={{width : "20%"}}>
-                helloasdoooooooooooo
+                <img src={logo} width="60" height="40" alt="logo image" />
             </div>
-            <div style={{width : "60%", textAlign: "center"}}>
-                hello
+            <div className='center-links'>
+                {
+                    ['New Releases', 'Trending', 'Collection']
+                    .map(
+                        link => (
+                            <h4 className='links grow'>{link}</h4>
+                        )
+                    )
+                }
             </div>
-            <div style={{width : "20%"}}>
-                hello
+            <div style={{width : "20%", display : "flex"}}>
+                <SearchBut />
+                <img />
             </div>
 
         </nav>
+    )
+}
+
+function SearchBut () {
+    return (
+        <div className='search'>
+            <img src={search} alt="search img" height="20" width="20" />
+            <h5 className='search-text'>search</h5>
+        </div> 
     )
 }
 
