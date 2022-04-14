0f8ca0269eaff6623845bb3528ebddc34d4fbad6
owati <mofiyinfoluwaowati@gmail.com>
2022-04-14 07:43:35 +0100

    working on the search UI

--- src/components/SearchModal.js
+++ src/components/SearchModal.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { getRequest } from "../functions/api";
 import Modal from "./Modal";
 import '../css/search.css'
@@ -7,15 +7,27 @@ import close from '../assets/close.png';
 import search from '../assets/search.png';
 
 function SearchModal({show, closed}) {
-    const [sku, setSku] = useState([]);
+    const [sku, setSku] = useState({
+        firstLoad : true, 
+        list : []
+    });
     const [searchWord, setWord] = useState("")
 
     async function handleSearch() {
-        const res = await getRequest('sku/search?q=' + searchWord);
+        const res = await getRequest('sku/search/suggest?q=' + searchWord);
         if (res.status === 200) {
-            setSku(res.data.data)
+            setSku(
+                {
+                    ...sku,
+                    firstLoad : false,
+                    list : res.data.data
+                }
+            )
         }
     }
+    useEffect(() => {
+        handleSearch();
+    }, [searchWord])
     return (
         <Modal show={show} direction={'modal-top'}>
             <div className="search-modal">
