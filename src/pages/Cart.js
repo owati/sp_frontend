3478a99e80adc1d9d60e11f1663c43298d389208
owati <mofiyinfoluwaowati@gmail.com>
2022-04-25 16:05:19 +0100

    worked on cart responsiveness

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -1,75 +1,168 @@
 import '../css/cart.css';
 import shirt from '../assets/shirt.png'
-import {ReactComponent as Delete} from '../assets/carbon_delete.svg';
-import {ReactComponent as Edit} from '../assets/carbon_edit.svg';
-
+import { ReactComponent as Delete } from '../assets/carbon_delete.svg';
+import { ReactComponent as Edit } from '../assets/carbon_edit.svg';
+import NumberInput from '../components/inputs/NumberInput';
+import SkuCard from '../components/SkuCard';
+import SkuCardList from '../components/SkuCardList';
 
 function Cart() {
     return (
         <div style={{
-            padding : "0px 20px"
+            padding: "0px 20px"
         }}>
             <div className='cart-header'>
                 <h1 style={{
-                    margin : "3px",
-                    fontStyle : "italic",
-                    fontWeight : "1000",
-                    width : "250px",
+                    margin: "3px",
+                    fontStyle: "italic",
+                    fontWeight: "1000",
+                    width: "250px",
                     textAlign: "center",
-                    borderBottom : "1px black solid"
+                    borderBottom: "1px black solid"
 
                 }}>My Cart</h1>
                 <h3 style={{
-                    margin : "0px",
+                    margin: "0px",
                     position: "absolute",
                     right: " 0px"
-                }}>Total: {10}</h3>
+                }} className="none-mobile">Total: {10}</h3>
             </div>
 
             <div className='cart-info'>
                 <div className='cart-details'>
-                    <div style={{
-                        display : "flex",
-                        height: "fit-content"
-                    }}>
-                        <div className='image-holder'>
-                            <img src={shirt} alt="pic" />
-                        </div>
+                    <div>
+                        <div style={{
+                            display: "flex",
+                            height: "fit-content",
+                            justifyContent: "space-between",
+                            borderBottom: '1px solid black',
+                            marginBottom: "40px"
+                        }}>
+                            <div className='image-holder'>
+                                <img src={shirt} alt="pic" />
+                            </div>
 
-                        <div className='cart-details-info'>
-                            <h3 style={{margin : "0px"}}>Galactic Rangers 2</h3>
-                            <h5>unisex summer shirt</h5>
-                            <h5>Color : black</h5>
-                            <h5>Size : M</h5>
+                            <div className='cart-details-info'>
+                                <h3 style={{ margin: "0px" }}>Galactic Rangers 2</h3>
+                                <h5>unisex summer shirt</h5>
+                                <h5>Color : black</h5>
+                                <h5>Size : M</h5>
 
-                            <button style={{
-                                display : "flex",
-                                border : "none",
-                                backgroundColor : "transparent",
-                                alignItems : "center",
-                                position : "absolute",
-                                bottom : "0px"
-                            }}>
-                                <Edit />
-                                <h5 style={{
-                                    margin : "0px",
+                                <button style={{
+                                    display: "flex",
+                                    border: "none",
+                                    backgroundColor: "transparent",
+                                    alignItems: "center",
+                                    position: "absolute",
+                                    bottom: "0px"
+                                }}>
+                                    <Edit />
+                                    <h5 style={{
+                                        margin: "0px",
 
-                                }}>Edit details</h5>
-                            </button>
+                                    }}>Edit details</h5>
+                                </button>
+
+                                <button  className="cart-del" style={{
+                                    border: "none",
+                                    backgroundColor: "transparent"
+                                }}>
+                                    <Delete />
+                                </button>
+                            </div>
+
+                            <div className='cart-details-info-2 none-mobile'>
+                                <h3 style={{
+                                    margin: "0px"
+                                }}>&#8358;10,000</h3>
+                                <div style={{
+                                    width: "100px"
+                                }}>
+                                    <NumberInput />
+                                </div>
+
+                                <button style={{
+                                    border: "none",
+                                    backgroundColor: "transparent"
+                                }}>
+                                    <Delete />
+                                </button>
+                            </div>
+
                         </div>
 
                     </div>
+                    <div className='cart-details'>
+                        <div style={{
+                            display: "flex",
+                            height: "fit-content",
+                            justifyContent: "space-between",
+                            borderBottom: '1px solid black',
+                        }}>
+                            <div className='image-holder'>
+                                <img src={shirt} alt="pic" />
+                            </div>
 
+                            <div className='cart-details-info'>
+                                <h3 style={{ margin: "0px" }}>Galactic Rangers 2</h3>
+                                <h5>unisex summer shirt</h5>
+                                <h5>Color : black</h5>
+                                <h5>Size : M</h5>
+
+                                <button style={{
+                                    display: "flex",
+                                    border: "none",
+                                    backgroundColor: "transparent",
+                                    alignItems: "center",
+                                    position: "absolute",
+                                    bottom: "0px"
+                                }}>
+                                    <Edit />
+                                    <h5 style={{
+                                        margin: "0px",
+
+                                    }}>Edit details</h5>
+                                </button>
+
+                                <button className="cart-del" style={{
+                                    border: "none",
+                                    backgroundColor: "transparent"
+                                }}>
+                                    <Delete />
+                                </button>
+                            </div>
+
+                            <div className='cart-details-info-2 none-mobile'>
+                                <h3 style={{
+                                    margin: "0px"
+                                }}>&#8358;10,000</h3>
+                                <div style={{
+                                    width: "100px"
+                                }}>
+                                    <NumberInput />
+                                </div>
+
+                                <button style={{
+                                    border: "none",
+                                    backgroundColor: "transparent"
+                                }}>
+                                    <Delete />
+                                </button>
+                            </div>
+
+                        </div>
+
+                    </div>
                 </div>
                 <div className='cart-checkout'>
-                    <h1 style={{margin : "0px"}}>Summary</h1>
+                    <h1 style={{ margin: "0px" }}>Summary</h1>
 
                     <div style={{
-                        display : "flex",
-                        alignItems : "center",
-                        justifyContent : "space-between",
-                        paddingRight : "30px",
-                        paddingTop : "40px",
+                        display: "flex",
+                        alignItems: "center",
+                        justifyContent: "space-between",
+                        paddingRight: "30px",
+                        paddingTop: "40px",
                         borderBottom: "2px solid rgba(0,0,0,0.4)"
                     }}>
                         <h3>Sub Total: </h3>
@@ -77,10 +170,10 @@ function Cart() {
 
                     </div>
                     <div style={{
-                        display : "flex",
-                        alignItems : "center",
-                        justifyContent : "space-between",
-                        paddingRight : "30px",
+                        display: "flex",
+                        alignItems: "center",
+                        justifyContent: "space-between",
+                        paddingRight: "30px",
                         borderBottom: "2px solid rgba(0,0,0,0.4)"
                     }}>
                         <h3>Total: </h3>
@@ -89,19 +182,19 @@ function Cart() {
                     </div>
 
                     <div style={{
-                        display : "flex",
-                        justifyContent : "center",
-                        paddingTop : "50px"
+                        display: "flex",
+                        justifyContent: "center",
+                        paddingTop: "50px"
                     }}>
                         <button className='grow shadow-5'
                             style={{
-                                width : "80%",
-                                height : "47px",
-                                backgroundColor : "black",
-                                color : "white",
+                                width: "80%",
+                                height: "47px",
+                                backgroundColor: "black",
+                                color: "white",
                                 border: "none",
-                                fontWeight : "bolder",
-                                borderRadius : "5px"
+                                fontWeight: "bolder",
+                                borderRadius: "5px"
                             }}
                         >Checkout</button>
 
@@ -111,6 +204,20 @@ function Cart() {
                 </div>
 
             </div>
+
+            <SkuCardList title="You might also like">
+                <SkuCard />
+                <SkuCard />
+                <SkuCard />
+                <SkuCard />
+                <SkuCard />
+                <SkuCard />
+                <SkuCard />
+                <SkuCard />
+                <SkuCard />
+                <SkuCard />
+                <SkuCard />
+            </SkuCardList>
 
         </div>
     )
