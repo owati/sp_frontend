ab2683750706ec3009ebb870db6aa07bf1d512af
owati <mofiyinfoluwaowati@gmail.com>
2022-05-31 23:32:32 +0100

    completed the checkout page

--- src/pages/Checkout.js
+++ src/pages/Checkout.js
@@ -1,10 +1,35 @@
+import { useState } from 'react';
+import Select from 'react-select';
 import { AddCardInput } from './PaymentInfo';
 import { ReactComponent as Cart } from '../assets/checkout.svg';
+import shirt from '../assets/shirt.png';
 import '../css/checkout.css';
-import { useState } from 'react';
 
 function Checkout() {
-    const [delivery, setDelivery] = useState(true)
+    const [checkoutData, setChekoutData] = useState({
+        delivery : true,
+        payment_method : 'paystack'
+    })
+
+    const {
+        delivery,
+        payment_method
+    } = 
+    
+    function setDelivery(value) {
+        setChekoutData({
+            ...checkoutData,
+            delivery : value
+        })
+    }
+
+    function setPayemtMethod(value) {
+        setChekoutData({
+            ...checkoutData,
+            payment_method : value
+        })
+    }
+
     return (
         <div>
             <header style={{
@@ -68,12 +93,12 @@ function Checkout() {
 
                             <div className={'checkout-delivery' + (delivery ? ' selected' : '')}>
                                 <h4 style={{ margin: 0 }}>Door delivery</h4>
-                                <input type='radio' name='delivery'  checked={delivery} onClick={() => setDelivery(true)}/>
+                                <input type='radio' name='delivery'  checked={delivery} onClick={() => console.log(delivery)}/>
                             </div>
 
                             <div className={'checkout-delivery' + (!delivery ? ' selected' : '')}>
                                 <h4 style={{ margin: 0 }}>Pick up</h4>
-                                <input type='radio' name='delivery' checked={!delivery} onClick={() => setDelivery(false)}/>
+                                <input type='radio' name='delivery' checked={!delivery} onClick={() =>  console.log(delivery)}/>
                             </div>
 
 
@@ -95,14 +120,85 @@ function Checkout() {
                             </div>
                             <h5 style={{ margin: 0 }}>PAYMENT METHOD</h5>
                         </div>
-                        <div className='checkout-form'>
+                        <div className='checkout-person'>
+                            
+                            <h4 style={{gridColumn : '1 / -1', margin : 0}}>Select the payment method: </h4>
+                            <Select 
+                            options={[
+                                {label : 'Use default card', value : 'card'},
+                                {label : 'Use another card ', value : 'nocard'}
+                            ]}
+                            onChange={
+                                e => {
+                                    setPayemtMethod(e.value)
+                                }
+                            }
+                            styles={{height : '100%'}}
+                            defaultValue={{
+                                label : 'Use default card', value : 'card'
+                            }}
+                            />
+                            <p style={{gridColumn : '1/ -1 ' }}>
+                                Payment 
+                            </p>
+                            
                             
                         </div>
-
                     </div>
 
                 </div>
                 <div className='checkout-payment'>
+                    <div className='checkout-payment-main shadow-5'>
+                        <h3>Order Checkout</h3>
+
+                        <div className='checkout-sku'>
+                            <div className='checkout-sku-img'>
+                                <img style={{margin : 'auto'}} src={shirt} alt='shirt'/>
+                            </div>
+                            <div className='checkout-sku-details'>
+                                <h3 style={{margin : '3px'}}>Galactic ranger</h3>
+                                <h3 style={{margin : '3px' , color : 'rgba(0,0,0,0.3)'}}>Color : black</h3>
+                                <h3 style={{margin : '3px' , color : 'rgba(0,0,0,0.3)'}}>Size : M</h3>
+                                <h3 style={{margin : '3px' , color : 'rgba(0,0,0,0.3)'}}>3 Units</h3>
+                                <h3 style={{margin : '3px'}}>N19,000</h3>
+                            </div>
+                        </div>
+
+                        <AddCardInput label='discount code' label_style={{backgroundColor : 'white'}}/>
+
+                        <div style={{display : 'flex', justifyContent : 'space-between', width : '100%'}}>
+                            <h4 style={{margin : '8px' , color : 'rgba(0,0,0,0.3)'}}>Shipping Cost:</h4>
+                            <h4 style={{margin : '8px'}}>22,000</h4>
+                        </div>
+                        <div style={{display : 'flex', justifyContent : 'space-between', width : '100%'}}>
+                            <h4 style={{margin : '8px' , color : 'rgba(0,0,0,0.3)'}}>Shipping Cost:</h4>
+                            <h4 style={{margin : '8px'}}>22,000</h4>
+                        </div>
+                        <div style={{display : 'flex', justifyContent : 'space-between', width : '100%'}}>
+                            <h4 style={{margin : '8px' , color : 'rgba(0,0,0,0.3)'}}>Shipping Cost:</h4>
+                            <h4 style={{margin : '8px'}}>22,000</h4>
+                        </div>
+                        <div style={{width : '100%', borderBottom : '1px solid black'}}></div>
+                        <div style={{display : 'flex', justifyContent : 'space-between', width : '100%'}}>
+                            <h4 style={{margin : '8px' , color : 'rgba(0,0,0,0.3)'}}>Shipping Cost:</h4>
+                            <h4 style={{margin : '8px'}}>22,000</h4>
+                        </div>
+
+                        <button className='grow shadow-5' style={{
+                            backgroundColor : 'black',
+                            color : 'white',
+                            borderRadius : '5px',
+                            border : 'none',
+                            width : '100%',
+                            height : '60px',
+                            marginTop : '20px',
+                            fontSize : '32px',
+                            fontWeight : 'bolder'
+                        }}>
+                            Checkout
+                        </button>
+
+                    </div>
 
                 </div>
 
