4f1eccaa6075f4dbe6067680b544168c632cae40
owati <mofiyinfoluwaowati@gmail.com>
2022-07-25 22:33:52 +0100

    changes in order view

--- src/pages/Order.js
+++ src/pages/Order.js
@@ -1,49 +1,87 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { toast } from "react-toastify";
 import shirt from '../assets/shirt.png'
+import { NoModalLoading } from "../components/Loading";
+import { getRequest } from "../functions/api";
+
+
+const status = {
+    placed : {name : 'new order', color : 'green'},
+    process : {name : 'processing', color : 'orange'},
+    delivery : {name : 'in transit', color : 'blue'},
+    done : {name : 'completed', color : 'red'}
+}
 
 
 function Order({ loading }) {
-    const [orders, setOrders] = useState(true)
+    const [orders, setOrders] = useState(null)
+    async function getOrders () {
+        const res = await getRequest('orders/user');
+        console.log(res)
+        if (res?.status === 200) {
+            setOrders(res.data.data.reverse())
+        } else {
+            toast.error('Erro while fetching the orders: ' + res?.data?.message || res?.message)
+        }
+    }
 
-    return (
+    useEffect(() => {
+        getOrders();
+    }, [])
+    return orders ? (
         <div>
             {
-                orders ? <div style={{ display: "flex", padding: "20px" }}>
+                orders.length > 0 ? <div style={{ display: "flex", padding: "20px" }}>
                     <div style={{ margin: "auto", width: "100%", maxWidth: "1000px" }}>
-                        <h3 style={{ margin: "0px" }}>Total: 4</h3>
+                        <h3 style={{ margin: "0px" }}>Total: {orders.length}</h3>
 
-                        <div className="order-container">
-                            <div className="order-div shadow-5 grow">
-                                <div className="order-time">
-                                    <h3 style={{ margin: "0px" }}>25</h3>
-                                    <h3 style={{ margin: "0px", fontWeight : "bolder" }}>June</h3>
-                                    <h5 style={{ margin: "0px" }}>2022</h5>
-                                </div>
-                                <div className="order-details">
-                                    <h1 style={{ margin: 0 }}>#012391</h1>
-                                    <h4 style={{ margin: 0 }}>5 Items</h4>
-                                </div>
-                                <div style={{
-                                    display : "flex",
-                                    flexDirection : "column",
-                                    alignItems : "center"
-                                }}>
-                                    <h4 style={{ margin: 0 }}>Total</h4>
-                                    <h2 style={{ margin: 0 }}>N90923</h2>
-                                </div>
-                                <div style={{
-                                    display : "flex",
-                                    justifyContent : "flex-end",
-                                    alignItems : "center"
-                                }}>
-                                    <h5 style={{
-                                        color : "orange",
-                                        margin : 0
-                                    }}>processing</h5>
-                                </div>
+                        {
+                            orders.map(
+                                order => {
+                                    console.log(order);
+                                    let totalCount = 0
+                                    order.order_list.forEach(item => {totalCount += item.data.quantity});
 
-                            </div>
-                        </div>
+                                    const date_list = new Date(order.date_created).toDateString().split(' ');
+                                    
+                                    const current_status = status[order.status[order.status.length - 1].status]
+                                    
+                                    return <div className="order-container">
+                                        <div className="order-div shadow-5 grow">
+                                            <div className="order-time">
+                                                <h3 style={{ margin: "0px" }}>{date_list[2]}</h3>
+                                                <h3 style={{ margin: "0px", fontWeight : "bolder" }}>{date_list[1]}</h3>
+                                                <h5 style={{ margin: "0px" }}>{date_list[3]}</h5>
+                                            </div>
+                                            <div className="order-details">
+                                                <h1 style={{ margin: 0 }}>#{order._id.slice(15)}</h1>
+                                                <h4 style={{ margin: 0 }}>{totalCount} Items</h4>
+                                            </div>
+                                            <div style={{
+                                                display : "flex",
+                                                flexDirection : "column",
+                                                alignItems : "center"
+                                            }}>
+                                                <h4 style={{ margin: 0 }}>Total</h4>
+                                                <h2 style={{ margin: 0 }}>&#8358;{order.finalCost.toLocaleString()}</h2>
+                                            </div>
+                                            <div style={{
+                                                display : "flex",
+                                                justifyContent : "flex-end",
+                                                alignItems : "center"
+                                            }}>
+                                                <h5 style={{
+                                                    color : current_status.color,
+                                                    margin : 0
+                                                }}>{current_status.name}</h5>
+                                            </div>
+            
+                                        </div>
+                                    </div>
+
+                                }    
+                            )        
+                        }
 
 
                     </div>
@@ -53,6 +91,12 @@ function Order({ loading }) {
                         <h1 style={{ margin: "auto", color: "rgba(0,0,0,0.3)" }}>You currently have no orders</h1>
                     </div>
             }
+        </div>
+    ) : (
+        <div style={{
+            height: '70vh'
+        }}>
+            <NoModalLoading />
         </div>
     )
 }
