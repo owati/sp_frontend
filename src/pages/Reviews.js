e5297fe2d2656cd3797debc7e8dba089a6a9fd08
owati <mofiyinfoluwaowati@gmail.com>
2022-07-31 22:52:10 +0100

    began the reviews integration

--- src/pages/Reviews.js
+++ src/pages/Reviews.js
@@ -1,44 +1,72 @@
+import { useEffect, useState } from 'react';
+import { useSelector } from 'react-redux';
 import { useParams, useNavigate } from 'react-router-dom';
 import '../css/reviews.css'
 import close from '../assets/close.png';
 import CustomRating from '../components/CustomRating';
-import {ReactComponent as Verified} from '../assets/verified.svg'
-import {ReactComponent as Check} from '../assets/check.svg'
-import {ReactComponent as NoCheck} from '../assets/nocheck.svg'
+import { ReactComponent as Verified } from '../assets/verified.svg'
+import { ReactComponent as Check } from '../assets/check.svg'
+import { ReactComponent as NoCheck } from '../assets/nocheck.svg'
+import { getRequest } from '../functions/api';
+import { toast } from 'react-toastify';
+import {NoModalLoading} from '../components/Loading'
 
 function Reviews() {
+    //const user  = useSelector(state => state.user.info);
+
     const navigate = useNavigate();
-    const {id} = useParams();
+    const { id } = useParams();
+
+    const [reviews, setReviews] = useState(null);
+
+    async function getReviews() {
+        const res = await getRequest('reviews?sku=' + id);
+        if (res?.status === 200) {
+            setReviews(res.data.data)
+        } else {
+            toast.error(res?.data?.message)
+        }
+    }
+
+    useEffect(() => {getReviews()}, [])
     return (
         <div className='review-div'>
             <button className='review-close grow'
-            onClick={
-                () => {
-                    navigate(-1);
-                }
-            }>
+                onClick={
+                    () => {
+                        navigate(-1);
+                    }
+                }>
                 <img src={close} alt="close" />
             </button>
             <div className='review-header'>
                 <h1>Galaxy's Reviews</h1>
-                <CustomRating initial={4} readonly/>
+                <CustomRating initial={4} readonly />
                 <button className='review-butt grow' onClick={
                     () => {
-                        navigate('../../writereview/pass')
+                        navigate('../../writereview/' + id)
                     }
                 }>
                     Write a review on this product
                 </button>
-                
-            </div>
-            <div style={{overflowY : 'scroll', height : 'calc(100% - 140px)'}}>
-            <ReviewCard />
-            <ReviewCard />
-            <ReviewCard />
-            <ReviewCard />
-            <ReviewCard />
 
-            </div>
+            </div> {
+                reviews ? 
+                    <div style={{ overflowY: 'scroll', height: 'calc(100% - 140px)' }}>
+                        {
+                            reviews?.reviews?.length ? 
+                            reviews?.reviews.map(
+                                review => {
+                                    return <ReviewCard data={review}/>
+                                }
+                            ) : <div style={{ height: '70vh', display : 'flex' }}>
+                                <h3 style={{margin : 'auto', color : 'rgba(0,0,0,0.5)'}}>There are currently no reviews for this rating</h3>
+                            </div>
+                        }
+                    </div> : <div style={{ height: '70vh' }}>
+                        <NoModalLoading />
+                    </div>
+            }
 
 
         </div>
@@ -48,7 +76,7 @@ function Reviews() {
 
 export default Reviews;
 
-function ReviewCard() {
+function ReviewCard({data}) {
     return (
         <div className='review-card'>
             <div className='profile-pic'>
@@ -57,32 +85,32 @@ function ReviewCard() {
             <div className='review-details'>
                 <div className='review-content'>
                     <div>
-                        <h3>Comfortable and stylish</h3>
-                        <CustomRating  red={false} initial={3} readonly/>
-                        <h3>Bashiru John</h3>
+                        <h3>{data?.headline}</h3>
+                        <CustomRating red={false} initial={data.rate} readonly />
+                        <h3>{data.user}</h3>
 
                     </div>
-                    <h4>November 21, 2021</h4>
+                    <h4>{data.date_created.toDateString()}</h4>
                 </div>
 
                 <p className='review-story'>lorem rerm er emr eremr emr em rme rme rme rme m ewmqmw emqw me mqw emq wme qmw emqw em qwme mqw emqwmeqmweqmwemqweqmwemqweqweqwe qweqwemqwe qwe qwe qwe qwe qwe qw eqwe we qw e </p>
                 <div className='review-metrics'>
                     <div className='review-metrics-content'>
                         <h4>Size: </h4>
-                        <h5>Accurate</h5>
+                        <h5>{data.size}</h5>
                     </div>
                     <div className='review-metrics-content'>
                         <h4>Comfort:</h4>
-                        <h5>Accurate</h5>
+                        <h5>{data.comfort}</h5>
                     </div>
                     <div className='review-metrics-content'>
                         <h4>Durability:</h4>
-                        <h5>Accurate</h5>
+                        <h5>{data.durability}</h5>
                     </div>
-                </div> 
+                </div>
                 <div className='review-card-base'>
                     <div>
-                        <h4 style={{display : 'inline-block', margin : 0}}>Was this helpful ?</h4>
+                        <h4 style={{ display: 'inline-block', margin: 0 }}>Was this helpful ?</h4>
                         <button className='review-butt-2 grow'>Yes <Check /></button>
                         <button className='review-butt-2 grow'>No <NoCheck /></button>
                     </div>
@@ -96,21 +124,21 @@ function ReviewCard() {
 }
 
 export function CreateReview() {
-    const {id} = useParams();
+    const { id } = useParams();
     const navigate = useNavigate();
     return (
-        <div className='review-div' style={{padding : "20px 0px"}}>
+        <div className='review-div' style={{ padding: "20px 0px" }}>
             <button className='review-close grow'
-            onClick={
-                () => {
-                    navigate(-1);
-                }
-            }>
+                onClick={
+                    () => {
+                        navigate(-1);
+                    }
+                }>
                 <img src={close} alt="close" />
             </button>
-            
+
             <div className='review-header' style={{
-                borderBottom : "1px solid black"
+                borderBottom: "1px solid black"
             }}>
                 <h1>Write A Review</h1>
                 <h4>Tell us how you feel about our product </h4>
@@ -122,20 +150,20 @@ export function CreateReview() {
                     e => {
                         console.log(e)
                     }
-                }/>
+                } />
 
                 <h3>Review headline</h3>
-                <textarea  className='review-header-input' placeholder='Give our product a unique description'/>
+                <textarea className='review-header-input' placeholder='Give our product a unique description' />
 
                 <h3>My Overall Review {'(optional)'}</h3>
-                <textarea  className='review-header-input-2' placeholder='Type exactly how you feel about this product. It must be atleast 50 characters long.'/>
-                
+                <textarea className='review-header-input-2' placeholder='Type exactly how you feel about this product. It must be atleast 50 characters long.' />
+
                 <h3>Size</h3>
-                <input type='range' min={0} max={100}/>
+                <input type='range' min={0} max={100} />
                 <div style={{
-                    display : "flex",
-                    alignItems : "center",
-                    justifyContent : "space-between"
+                    display: "flex",
+                    alignItems: "center",
+                    justifyContent: "space-between"
                 }}>
                     <h5 className='review-range-text'>Small</h5>
                     <h5 className='review-range-text'>Accurate</h5>
@@ -145,9 +173,9 @@ export function CreateReview() {
                 <h3>Comfort</h3>
                 <input type='range' min={0} max={100} />
                 <div style={{
-                    display : "flex",
-                    alignItems : "center",
-                    justifyContent : "space-between"
+                    display: "flex",
+                    alignItems: "center",
+                    justifyContent: "space-between"
                 }}>
                     <h5 className='review-range-text'>Not Comfortable</h5>
                     <h5 className='review-range-text'>Average</h5>
@@ -155,11 +183,11 @@ export function CreateReview() {
                 </div>
 
                 <h3>Durability</h3>
-                <input type='range' min={0} max={100}/>
+                <input type='range' min={0} max={100} />
                 <div style={{
-                    display : "flex",
-                    alignItems : "center",
-                    justifyContent : "space-between"
+                    display: "flex",
+                    alignItems: "center",
+                    justifyContent: "space-between"
                 }}>
                     <h5 className='review-range-text'>Not Durable</h5>
                     <h5 className='review-range-text'>Average</h5>
@@ -167,7 +195,7 @@ export function CreateReview() {
                 </div>
 
 
-                <div style={{display : "flex", marginBottom : "40px", marginTop : "20px"}}>
+                <div style={{ display: "flex", marginBottom: "40px", marginTop: "20px" }}>
                     <button className='review-post-butt shadow-5 grow'>Post Review</button>
                 </div>
             </div>
